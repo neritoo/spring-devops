1f5a3f6adf8a8a4ec3c8a06f454560454905ff9a
deofis <deofis.github@gmail.com>
2020-10-14 23:43:49 -0300

    MySQL added.

---
+++ src/main/java/guru/springframework/config/QaEncryptedConfig.java
@@ -0,0 +1,12 @@
+package guru.springframework.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@EncryptablePropertySource(name = "qaEncryptedProperties", value = "classpath:qa.encrypted.properties")
+@Profile("qa")
+public class QaEncryptedConfig {
+
+}
---
+++ src/test/java/guru/springframework/test/ds/FakeDataSource.java
@@ -0,0 +1,6 @@
+package guru.springframework.test.ds;
+
+public interface FakeDataSource {
+
+    String getConnectionInfo();
+}
